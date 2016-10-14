8b006cf440af167c6f86861ae5fa7014b56fb7d4
nk-mandilwar <nitinkumarmandilwar5@gmail.com>
2016-10-14 18:29:54 +0530

    adding multiple questions with options

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -1,2 +1,22 @@
 class OptionsController < ApplicationController
+	before_action :authenticate_user!
+
+	def create
+		@option_id = option_params[:option_id]
+		@option = Option.new(create_option_params)
+		@option.save
+		respond_to do |format|
+			format.js
+		end
+	end
+
+	private
+
+		def option_params
+			params.require(:option).permit(:answer, :survey_id, :question_id, :option_id)
+		end	
+
+		def create_option_params
+			option_params.slice(:answer, :survey_id, :question_id)
+		end	
 end
--- app/models/option.rb
+++ app/models/option.rb
@@ -1,4 +1,6 @@
 class Option < ActiveRecord::Base
   belongs_to :survey
   belongs_to :question
+
+  validates :answer, presence: true, length: {maximum: 50}
 end
